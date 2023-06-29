dd0c17005f24579ae115009ad023322b826e8f5a
Aniket Daripa <aniketdaripa84@gmail.com>
2023-06-30 00:10:52 +0530

    2

--- chatServer.js
+++ chatServer.js
@@ -4,7 +4,7 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
-
+//
 const server = http.createServer(app);
 
 const io = new Server(server, {
