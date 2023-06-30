10a817528600f14a149611e4d9c55723997b2634
Aniket Daripa <aniketdaripa84@gmail.com>
2023-06-30 08:59:21 +0530

    chat backend final

--- chatServer.js
+++ chatServer.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://touristwebapp.netlify.app/",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
